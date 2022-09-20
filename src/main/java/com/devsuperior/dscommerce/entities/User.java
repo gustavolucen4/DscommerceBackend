0d71202cbad3e75a1a49ee96c64561540afd3155
Gustavo Lucena <gustavolucena12@gmail.com>
2022-09-20 09:14:47 -0300

    Entity Order

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +19,9 @@ public class User {
     private LocalDate birthDate;
     private String password;
     private String roles;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }

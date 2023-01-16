edb7eaf3a518f581668adcd8ac19e74b2eede85f
Gustavo Lucena <gustavolucena12@gmail.com>
2023-01-16 10:35:14 -0300

    Oaut2 jwt check list

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,7 +1,11 @@
 package com.devsuperior.dscommerce;
 
+import com.devsuperior.dscommerce.config.WebSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DscommerceApplication {
@@ -10,5 +14,4 @@ public class DscommerceApplication {
 
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
-
 }
